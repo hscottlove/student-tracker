bb66972619b40dca9105ca75cbca7c6a9adacb38
Scott Love <scottlove@Claudias-MacBook-Pro.local>
2021-12-20 13:41:06 -0800

    added student images for cardlist component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 export const StudentContext = React.createContext();
 
 function App() {
-  const [selectedStudentId, setSelectedStudentId] = useState();
+  // const [selectedStudentId, setSelectedStudentId] = useState();
   const [students, setStudents] = useState(data);
   const [search, setSearch] = useState('');
 
@@ -25,6 +25,7 @@ function App() {
     const newStudent = {
       id: uuidv4(),
       name: 'name test',
+      image: 'https://freesvg.org/img/abstract-user-flat-4.png',
       email: 'email test',
       phone: 'number test',
     };
@@ -53,20 +54,29 @@ const data = [
   {
     id: 1,
     name: 'Marcelo Garcia',
+    image:
+      'https://images.unsplash.com/photo-1618827840222-fcf8f42509c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHByb2ZpbGUlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     email: 'mgarcia@gmail.com',
     phone: 9256496486,
+    notes: 'Schedual a meeting Monday to talk about grades.',
   },
   {
     id: 2,
     name: 'Amanda Love',
+    image:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60',
     email: 'amanda@gmail.com',
     phone: 9259496020,
+    notes: 'Ask about project outline.',
   },
   {
     id: 3,
     name: 'Jim Jones',
+    image:
+      'https://images.unsplash.com/photo-1601233749202-95d04d5b3c00?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHByb2ZpbGUlMjBpbWFnZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60',
     email: 'jimjones77@gmail.com',
     phone: 7136476020,
+    notes: 'Email extra credit opportunities.',
   },
 ];
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,17 +2,15 @@ import React, { useContext } from 'react';
 import { Button, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { StudentContext } from '../App';
 
-export default function CardList({ id, name, email, phone }) {
+export default function CardList({ id, name, image, email, phone, notes }) {
   const { handleStudentDelete } = useContext(StudentContext);
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img
-        variant='top'
-        src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cHJvZmlsZSUyMGltYWdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=60'
-      />
+      <Card.Img variant='top' style={{ height: '100%' }} src={image} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
-        <Card.Text>Schedual a meeting Monday to talk about grades.</Card.Text>
+        <hr />
+        <Card.Text>Notes: {notes}</Card.Text>
       </Card.Body>
       <ListGroup className='list-group-flush'>
         <ListGroupItem>Email: {email}</ListGroupItem>
