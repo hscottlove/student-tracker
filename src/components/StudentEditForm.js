48734076d3a7208fe53b3789898dc1e9a4c5f3e8
Scott Love <scottlove@Claudias-MacBook-Pro.local>
2021-12-22 11:13:35 -0800

    updated studenteditform inputs for edit button

--- src/components/StudentEditForm.js
+++ src/components/StudentEditForm.js
@@ -46,28 +46,37 @@ export default function StudentForm({ student }) {
             />
           </Form.Group>
 
-          <Form.Group as={Col} controlId='formGridEmail'>
+          <Form.Group as={Col} htmlFor='email'>
             <Form.Label>Email</Form.Label>
             <Form.Control
               onChange={(e) => handleChange({ email: e.target.value })}
               type='email'
+              name='email'
+              id='email'
+              value={student.email}
             />
           </Form.Group>
 
-          <Form.Group as={Col} controlId='formGridNumber'>
+          <Form.Group as={Col} htmlFor='grade'>
             <Form.Label>Grade</Form.Label>
             <Form.Control
               onChange={(e) => handleChange({ grade: e.target.value })}
               type='number'
+              name='grade'
+              id='grade'
+              value={student.grade}
             />
           </Form.Group>
         </Row>
 
-        <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+        <Form.Group className='mb-3' htmlFor='notes'>
           <Form.Label>Notes</Form.Label>
           <Form.Control
             onChange={(e) => handleChange({ notes: e.target.value })}
             style={{ resize: 'none' }}
+            name='notes'
+            id='notes'
+            value={student.notes}
             as='textarea'
             rows={3}
           />
